245d186d0fd06fed5dbac432f883ab89f9cc6793
Finja <finja@poelking.info>
2026-08-05 13:47:45 +0200

    Realistische Daten und Sensorzuordnung

--- tools/DataGenerator.py
+++ tools/DataGenerator.py
@@ -10,20 +10,17 @@ BASE_PATH = path = os.path.dirname(os.path.dirname(__file__))
 class SensorKey:
     """Unique key of a sensor"""
 
-    def __init__(self, name: str, type: str):
+    def __init__(self, name: str):
         """Constructor"""
         self.name: str = name
-        self.type: str = type
-
 
 class Sensor:
     """Sensor object, which stores all information of a given sensor."""
 
-    def __init__(self, name: str, type: str, pressure: float | None, temperature: float | None):
+    def __init__(self, name: str, pressure: float | None, temperature: float | None):
         """Constructor"""
 
         self.name: str = name
-        self.type: str = type
         self.pressure: float | None = pressure
         self.temperature: float | None = temperature
 
@@ -34,34 +31,43 @@ class DataGenerator:
     def __init__(self):
         """Constructor"""
         self.available_sensors: list[SensorKey] = [
-            SensorKey(name="thruster_1.a", type="thruster"),
-            SensorKey(name="thruster_1.b", type="thruster"),
-            SensorKey(name="thruster_1.c", type="thruster"),
-            SensorKey(name="thruster_2.a", type="thruster"),
-            SensorKey(name="thruster_2.b", type="thruster"),
-            SensorKey(name="thruster_2.c", type="thruster"),
-            SensorKey(name="thruster_3.a", type="thruster"),
-            SensorKey(name="thruster_3.b", type="thruster"),
-            SensorKey(name="thruster_3.c", type="thruster"),
-            SensorKey(name="oxygen_tank_1", type="gas_valve"),
-            SensorKey(name="oxygen_tank_2", type="gas_valve"),
-            SensorKey(name="hydrogen_tank_1", type="gas_valve"),
-            SensorKey(name="hydrogen_tank_2", type="gas_valve")
+            SensorKey(name="thruster_1.a"),
+            SensorKey(name="oxygen_tank_1"),
+            SensorKey(name="hydrogen_tank_1")
         ]
 
     def generate_new_sensor_data(self):
 
+        druckH = random.uniform(1.50, 3.50)
+        temperaturH = random.uniform(14.00, 20.15)
+        druckO = random.uniform(1.50, 3.50)
+        temperaturO = random.uniform(90.00, 95.00)
+        temperaturA = random.uniform(2800, 3500)
+        druckA = random.uniform(1.0, 3.0)
+        # entfernung  = random.uniform(160.00, 2000.00)
+
         selected_key_idx = random.randint(0, len(self.available_sensors) - 1)
         selected_key = self.available_sensors[selected_key_idx]
 
-        pressure = random.uniform(0.5, 9.0)
-        temperature = random.uniform(200.0, 500.0)
+        if selected_key.name == "hydrogen_tank_1": 
+            sensor_data = Sensor(
+                name=selected_key.name,
+                pressure=druckH, 
+                temperature=temperaturH
+        )
+     
+        elif selected_key.name == "oxygen_tank_1":
+            sensor_data = Sensor(
+                name=selected_key.name,
+                pressure=druckO,
+                temperature=temperaturO
+        )
 
-        sensor_data = Sensor(
-            name=selected_key.name,
-            type=selected_key.type,
-            pressure=pressure,
-            temperature=temperature
+        else:
+            sensor_data = Sensor(
+                name=selected_key.name,
+                pressure=druckA,
+                temperature=temperaturA
         )
 
         return sensor_data
@@ -81,4 +87,4 @@ if __name__ == '__main__':
         data = generator.generate_new_sensor_data()
         generator.store_sensor_data(data=data)
         print(f"Sucessfully stored: {data}")
-        time.sleep(2)
+        time.sleep(random.randint(1, 10))  # Sleep for a random time between 1 and 10 seconds
