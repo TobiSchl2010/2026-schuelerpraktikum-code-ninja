d90fe5fe12e05cfa97f7aea3ef4dc37489cb210b
TobiSchl2010 <tobiasschlagbauer1@gmail.com>
2026-08-06 09:55:21 +0200

    Delete unused files

--- add_data.py
+++
@@ -1,5 +0,0 @@
-from processing.processing import process_json_folder
-
-if __name__ == "__main__":
-    folder_path = "data"  # Replace with your actual folder path
-    process_json_folder(folder_path)
